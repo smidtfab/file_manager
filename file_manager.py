3c3677e7bbc915bf042b33bd1d77c510013810fc
Fabian Schmidt <fabis@outlook.com>
2020-04-24 16:51:55 +0200

    Closes #1 append file counter if duplicate is detected

--- file_manager.py
+++ file_manager.py
@@ -44,12 +44,40 @@ def createDirs():
                 continue
 
 # function to move certain file to a given path
-def move_file(old_file, file_path):
-    try:
-        os.rename(old_file, file_path)
-        print(f"moved file {old_file} >> {file_path}")
-    except (OSError, IndexError):
-        print(f"could not move file {old_file} >> {file_path}")
+def move_file(old_file_path, new_file_path):
+    # check if new_file_path already exists
+    if os.path.exists(new_file_path):
+        # rename the duplicate file
+        converted_path = PurePath(new_file_path)
+
+        # split path into stem e.g. test and duplicate counter __
+        path_split = converted_path.stem.split(sep="__")
+        name_stem = path_split[0]
+
+        try:
+            # get counter and incrementally increase
+            rec_count = int(path_split[1])
+        except IndexError:
+            # except when first run of recursion then set 0
+            rec_count = 0
+        
+        #increase counter for next recursion
+        rec_count = rec_count + 1
+
+        # create new file name consisten of parent e.g. /parent, file name e.g. test, counter e.g. 2 and suffix e.g. .py
+        adapted_file_path = str(converted_path.parent) + '/' + name_stem + '__' + str(rec_count) + str(converted_path.suffix)
+
+        # run move_file with adapted file_name until file_name is unique
+        move_file(old_file_path, adapted_file_path)
+    else:
+        # stopping condition for recursion when file name is unique
+        try:
+            # move file from old location to new unique location 
+            os.rename(old_file_path, new_file_path)
+            print(f"moved file {old_file_path} >> {new_file_path}")
+        except (OSError, IndexError):
+            # handle errors
+            print(f"could not move file {old_file_path} >> {new_file_path}")
 
 # function to move files to respective folders
 def arrange():
