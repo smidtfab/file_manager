05a0d45265b9c39adb09e5e3633ed52bb9ac12a4
Fabian Schmidt <fabis@outlook.com>
2020-05-09 15:22:59 +0200

    Closes #4 duplicate files

--- set_listener.sh
+++ set_listener.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-DIR="/home/fabian/Downloads/test/"
+DIR="/home/fabian/Downloads/"
 inotifywait -m -r -e create "$DIR" | while read f
 
 do
     # insert detected
     echo change in folder detected, running python script ...
+    # wait for file creation (was a problem with pdfs)
+    sleep 1.5
     # run python file manager
     python3 file_manager.py
 done
